3f2ea2636a78fa644da6a5def36923b995b4cc9d
Jamie Warner <jamiesonwarner@gmail.com>
2017-02-18 19:55:30 +0000

    working library

--- pi/test.py
+++ pi/test.py
@@ -4,29 +4,30 @@ import numpy as np
 import cv2
 
 class RaspberryPI:
-	resolution = (1920, 1088)
 	camera = picamera.PiCamera()
+	buffer_res = (1920, 1088)
+	camera_res = (1920, 1080)
 
 	def __init__(self):
-		# self.camera.resolution = (100, 100)
+		self.camera.resolution = self.camera_res
 		# self.camera.framerate = 24
 		time.sleep(1)
-		print self.camera.resolution
 
 	def take_picture(self):
-		buf_size = self.resolution[0] * self.resolution[1] * 3
+		buf_size = self.buffer_res[0] * self.buffer_res[1] * 3
 		output = np.empty((buf_size,), dtype=np.uint8)
-		self.camera.capture(output, 'rgb')
-		output = output.reshape((self.resolution[0], self.resolution[1], 3))
-		# output = output[:100, :100, :]
-
-		cv2.namedWindow('dst_rt', cv2.WINDOW_NORMAL)
-		cv2.imshow('dst_rt', output)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+		self.camera.capture(output, 'bgr')
+		output = output.reshape((self.buffer_res[1], self.buffer_res[0], 3))
+		return output
 	
+def show_image(im):
+	cv2.namedWindow('im', cv2.WINDOW_NORMAL)
+	cv2.imshow('im', im)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-	print('hello world')
+	print('Taking picture...')
 	pi = RaspberryPI()
-	pi.take_picture()
+	im = pi.take_picture()
+	show_image(im)
